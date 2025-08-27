140ce456b85bddf89596322bf859a7b61083056a
JoshuaJeyaranjan <141107926+JoshuaJeyaranjan@users.noreply.github.com>
2025-08-27 07:42:01 -0400

    hero page initial setup

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './HomePage.scss'
 import Nav from '../../Components/Nav/Nav'
+import { Link } from 'react-router-dom'
 const HomePage = () => {
     return (
-        <div>
+        <>
             <Nav/>
-            <p>Home</p>
-        </div>
+            <div className='home'>
+            
+            <h1 className='home__title text'>DFS Vision</h1>
+            <h3 className='home__subtitle text'>Videographer & Photographer</h3>
+            <Link className='home__link' to='/video' > <p>View Work</p> </Link>
+            </div>
+            
+        </>
     )
 }
 
